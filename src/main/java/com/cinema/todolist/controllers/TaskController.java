d6ca4070d55ee63c1f173dc8fefee1970a5d3db2
Belghieti <mohamedbelghieti1@gmail.com>
2025-07-05 01:59:37 +0200

    Dev22 :add PostController

--- src/main/java/com/cinema/todolist/controllers/TaskController.java
+++ src/main/java/com/cinema/todolist/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package com.cinema.todolist.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,5 +12,9 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<String> getTasks() {
         return List.of("Tâche 1", "Tâche 2", "Tâche 3");
+    }
+    @PostMapping("/test")
+    public String test() {
+        return "test";
     }
 }
